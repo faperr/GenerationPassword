87e3102ba998b4121e8362a0d00a64d06b10d1ce
faperr <gvyu371@gmail.com>
2025-09-30 13:38:36 +0300

    Добавьте файлы проекта.

---
+++ GenerationPassword/Form1.Designer.cs
@@ -0,0 +1,79 @@
+﻿namespace GenerationPassword
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox = new System.Windows.Forms.TextBox();
+            this.BtnGeneration = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox
+            // 
+            this.textBox.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox.Location = new System.Drawing.Point(250, 250);
+            this.textBox.Name = "textBox";
+            this.textBox.ReadOnly = true;
+            this.textBox.Size = new System.Drawing.Size(250, 31);
+            this.textBox.TabIndex = 0;
+            // 
+            // BtnGeneration
+            // 
+            this.BtnGeneration.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.BtnGeneration.Location = new System.Drawing.Point(285, 300);
+            this.BtnGeneration.Name = "BtnGeneration";
+            this.BtnGeneration.Size = new System.Drawing.Size(190, 35);
+            this.BtnGeneration.TabIndex = 1;
+            this.BtnGeneration.Text = "Сгенерировать";
+            this.BtnGeneration.UseVisualStyleBackColor = true;
+            this.BtnGeneration.Click += new System.EventHandler(this.BtnGeneration_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.ClientSize = new System.Drawing.Size(782, 553);
+            this.Controls.Add(this.BtnGeneration);
+            this.Controls.Add(this.textBox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox;
+        private System.Windows.Forms.Button BtnGeneration;
+    }
+}
+
---
+++ GenerationPassword/Form1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+using System.Windows.Forms;
+
+namespace GenerationPassword
+{
+    public partial class Form1 : Form
+    {
+        private static readonly RandomNumberGenerator Rng = RandomNumberGenerator.Create();
+
+        private readonly char[] lower = "abcdefghijklmnopqrstuvwxyz".ToCharArray();
+        private readonly char[] upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".ToCharArray();
+        private readonly char[] digits = "0123456789".ToCharArray();
+        private readonly char[] allChars;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            // собрать набор символов
+            var list = new StringBuilder();
+            list.Append(lower);
+            list.Append(upper);
+            list.Append(digits);
+            allChars = list.ToString().ToCharArray();
+        }
+
+        private void BtnGeneration_Click(object sender, EventArgs e)
+        {
+            string password = GeneratePassword(12); // длина по умолчанию 12
+            textBox.Text = password;
+        }
+
+        private string GeneratePassword(int length)
+        {
+            if (length < 3)
+                throw new ArgumentException("Длина должна быть не меньше 3");
+
+            char[] result = new char[length];
+
+            // гарантируем хотя бы по одному символу из каждой группы
+            result[0] = PickRandom(lower);
+            result[1] = PickRandom(upper);
+            result[2] = PickRandom(digits);
+
+            for (int i = 3; i < length; i++)
+                result[i] = PickRandom(allChars);
+
+            Shuffle(result);
+            return new string(result);
+        }
+
+        private char PickRandom(char[] arr)
+        {
+            int idx = GetRandomInt(0, arr.Length);
+            return arr[idx];
+        }
+
+        private void Shuffle(char[] array)
+        {
+            for (int i = array.Length - 1; i > 0; i--)
+            {
+                int j = GetRandomInt(0, i + 1);
+                (array[i], array[j]) = (array[j], array[i]);
+            }
+        }
+
+        // Реализация RandomNumberGenerator.GetInt32 для .NET Framework
+        private int GetRandomInt(int minInclusive, int maxExclusive)
+        {
+            if (minInclusive >= maxExclusive)
+                throw new ArgumentOutOfRangeException();
+
+            uint range = (uint)(maxExclusive - minInclusive);
+            uint limit = uint.MaxValue - (uint.MaxValue % range);
+
+            byte[] buffer = new byte[4];
+            while (true)
+            {
+                Rng.GetBytes(buffer);
+                uint rand = BitConverter.ToUInt32(buffer, 0);
+                if (rand < limit)
+                    return (int)(minInclusive + (rand % range));
+            }
+        }
+    }
+}
